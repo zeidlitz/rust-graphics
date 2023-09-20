ed5a2847abf2dde83745d2b97ac90e0ebb94a82c
IcookTacos <zeidlitzerik@gmail.com>
2023-09-20 20:22:03 +0200

    Dependecies & API tests
    - Added packages and dependencies to Cargo.toml
    - Testing out the wgpu API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let instances = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
+    for adapter in instances.enumerate_adapters(wgpu::Backends::all()){
+        println!("{:?}", adapter.get_info());
+    }
 }
